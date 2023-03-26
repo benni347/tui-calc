1cb3c09eb234640138db5d9f4da94436b543d27a
Cédric Skwar <cdrc@skwar.me>
2023-03-26 21:05:04 +0200

    Added a todo

--- src/cone.rs
+++ src/cone.rs
@@ -10,5 +10,7 @@ pub fn cone(r: f64, h: f64) -> f64 {
 pub fn cone_surface_area(r: f64, h: f64) -> f64 {
     // This function calculates the surface area of a cone
     // https://mathworld.wolfram.com/Cone.html
+    // TODO: As soon as I have the pythagorean theorem, I can use it here
+    // instead of the sqrt function
     circle(r) + pi() * r * (r.powi(2) + h.powi(2)).sqrt()
 }
